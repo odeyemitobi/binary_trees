8696403987a61c95e4c511e082f3439c60305fe2
odeyemitobi <odeyemioluwatobiloba11@gmail.com>
2024-03-26 23:27:07 +0100

    Task 16 error fixing

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -27,28 +27,33 @@ size_t binary_tree_height(const binary_tree_t *tree)
  */
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
-	size_t height = 0, nodes = 0, perfect_nodes = 0;
+	int height, nodes, expected_nodes;
 
 	if (tree == NULL)
 		return (0);
 
 	height = binary_tree_height(tree);
 	nodes = binary_tree_size(tree);
-	perfect_nodes = (1 << height) - 1;
+	expected_nodes = (1 << (height + 1)) - 1;
 
-	return (nodes == perfect_nodes);
+	return (nodes == expected_nodes);
 }
 
 /**
  * binary_tree_size - Measures the size of a binary tree
  * @tree: Pointer to the root node of the tree to measure the size
  *
- * Return: Size of the tree
+ * Return: Size of the binary tree, 0 if tree is NULL
  */
 size_t binary_tree_size(const binary_tree_t *tree)
 {
+	size_t size_left, size_right;
+
 	if (tree == NULL)
 		return (0);
 
-	return (1 + binary_tree_size(tree->left) + binary_tree_size(tree->right));
+	size_left = binary_tree_size(tree->left);
+	size_right = binary_tree_size(tree->right);
+
+	return (1 + size_left + size_right);
 }
