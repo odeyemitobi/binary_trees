dca490189ad0d0cb6a9f4dc22cde609e8e777e1d
odeyemitobi <odeyemioluwatobiloba11@gmail.com>
2024-03-27 14:11:15 +0100

    Write a function that inserts a value in a Binary Search Tree

--- binary_trees.h
+++ binary_trees.h
@@ -19,6 +19,7 @@ struct binary_tree_s
 };
 
 typedef struct binary_tree_s binary_tree_t;
+typedef struct binary_tree_s bst_t;
 
 /* Function prototypes */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
