4b90dbdbc751549aa6befbe0db1e1cd40e05bfea
odeyemitobi <odeyemioluwatobiloba11@gmail.com>
2024-03-27 11:13:20 +0100

    Write a function that finds the lowest common ancestor of two nodes

--- 100-binary_trees_ancestor.c
+++ 100-binary_trees_ancestor.c
@@ -8,7 +8,7 @@
  * Return: Pointer to the lowest common ancestor node, or NULL if not found
  */
 binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
-									 const binary_tree_t *second)
+const binary_tree_t *second)
 {
 	if (first == NULL || second == NULL)
 		return (NULL);
